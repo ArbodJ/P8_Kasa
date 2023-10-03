7782dc2d0427d56a8371fc7fd8761b957c445188
ArbodJ <ajiissa38@gmail.com>
2023-10-03 17:26:17 +0200

    Correction de texte de nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,10 +10,10 @@ const Navigation = () => {
       <div className="link-container">
         <ul className='ul-link-pages'>
           <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-            <li className='li-link-page'>ACCUEIL</li>
+            <li className='li-link-page'>Accueil</li>
           </NavLink>
           <NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-            <li className='li-link-page'>A PROPOS</li>
+            <li className='li-link-page'>A Propos</li>
           </NavLink>
         </ul>
       </div>
